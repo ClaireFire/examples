8c01218ab0e257219fec055c56dac2c9daecfaad
yangwei <654183746@qq.com>
2015-08-12 09:40:15 +0800

    20150812

---
+++ Task/src/com/example/task/Activity_3.java
@@ -0,0 +1,36 @@
+package com.example.task;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class Activity_3 extends BaseActivity implements OnClickListener{
+	 Button lookTaskInfo;
+	 public static final String TAG = "young Activity_3";
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		lookTaskInfo = (Button)this.findViewById(R.id.lookTaskInfo);
+		lookTaskInfo.setOnClickListener(this);
+		lookTaskInfo.setText(TAG);
+	}
+	
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		int tag = v.getId();
+		switch (tag) {
+		case  R.id.lookTaskInfo:
+			Intent intent = new Intent(this, Activity_1.class);
+			startActivity(intent);
+			showRunningTasks(TAG);
+			break;
+			
+		default:
+			break;
+		}
+	}  
+}
